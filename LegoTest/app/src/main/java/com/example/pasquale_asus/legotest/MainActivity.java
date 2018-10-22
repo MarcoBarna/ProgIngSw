56676a9014ca2e6e9b6b9ecc301d9c27425503ca
@aboutpask <passpolo00@gmail.com>
2018-10-22 19:52:15 +0200

    XML now Available

--- LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/MainActivity.java
+++ LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/MainActivity.java
@@ -1,21 +1,38 @@
 package com.example.pasquale_asus.legotest;
+import android.view.View;
+import android.widget.Button;
+import com.google.appinventor.components.runtime.BluetoothClient;
+import com.google.appinventor.components.runtime.Ev3Motors;
+import com.google.appinventor.components.runtime.Form;
+import com.google.appinventor.components.runtime.ListPicker;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-import com.google.appinventor.components.runtime.NxtDrive;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class MainActivity extends Form
+{
+    private Ev3Motors motor1;
+    private int numbers = 0;
+    private ListPicker ListPicker1;
+    private BluetoothClient BluetoothClient1;
+    private Button button;
+    // $define is where you'll create components, initialize properties and make any calls that
+    // you'd put in Screen.Initialize of an App Inventor app
+    protected void $define()
+    {
         setContentView(R.layout.activity_main);
-        $define();
+        button = findViewById(R.id.button);
+        ListPicker1 = new ListPicker(this);
+        ListPicker1.Text("Connect");
+        BluetoothClient1 = new BluetoothClient(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonClicked();
+            }
+        });
+        motor1 = new Ev3Motors(this);
     }
-    protected void $define() {
-
+    public void buttonClicked(){
+        numbers += 1;
+        button.setText("Clicked " + numbers + " time");
     }
 
 }
---
+++ LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/old_MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pasquale_asus.legotest;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class old_MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                button.setText("CIAO");
+                // Code here executes on main thread after user presses button
+            }
+        });
+    }
+
+
+}
+
