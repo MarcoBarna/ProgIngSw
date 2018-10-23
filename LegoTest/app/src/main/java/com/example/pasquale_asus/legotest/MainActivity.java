83591c7b42c7bdfa45f42c55f15873e7d29623f6
@aboutpask <passpolo00@gmail.com>
2018-10-23 13:51:28 +0200

    Button Bluethoot

--- LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/MainActivity.java
+++ LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.pasquale_asus.legotest;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import com.google.appinventor.components.runtime.BluetoothClient;
 import com.google.appinventor.components.runtime.Ev3Motors;
 import com.google.appinventor.components.runtime.Form;
 import com.google.appinventor.components.runtime.ListPicker;
+import com.google.appinventor.components.runtime.util.YailList;
 
 
 public class MainActivity extends Form
@@ -14,25 +17,42 @@ public class MainActivity extends Form
     private ListPicker listPicker1;
     private BluetoothClient BluetoothClient1;
     private Button button;
+    private Button buttonBluethoot;
+    private TextView text;
     // $define is where you'll create components, initialize properties and make any calls that
     // you'd put in Screen.Initialize of an App Inventor app
     protected void $define()
     {
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
+        buttonBluethoot = findViewById(R.id.buttonBluethoot);
+        text = findViewById(R.id.textView);
         listPicker1 = new ListPicker(this);
         listPicker1.Text("Connect");
         BluetoothClient1 = new BluetoothClient(this);
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 buttonClicked();
             }
         });
+        buttonBluethoot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonBluethootClicked();
+            }
+        });
+
         motor1 = new Ev3Motors(this);
     }
     public void buttonClicked(){
         numbers += 1;
-        button.setText("Clicked " + numbers + " time");
+        button.setText("Clicked " + numbers + " time" + listPicker1.Selection());
+    }
+    public void buttonBluethootClicked(){
+        listPicker1.Elements(YailList.makeList(BluetoothClient1.AddressesAndNames()));
+        BluetoothClient1.Connect(listPicker1.Selection());
+        buttonBluethoot.setText(listPicker1.Selection());
     }
 
 }
