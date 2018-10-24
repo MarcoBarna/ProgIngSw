3cbbf7bcdcaa818bad748d5e3e0471d935b71cde
@aboutpask <passpolo00@gmail.com>
2018-10-24 16:48:50 +0200

    BtPaired

--- LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/BtPaired.java
+++ LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/BtPaired.java
@@ -2,14 +2,16 @@ package com.example.pasquale_asus.legotest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
-import com.google.appinventor.components.runtime.Form;
-import com.google.appinventor.components.runtime.ListPicker;
-
-
-public class BtPaired extends Form {
-    private ListPicker listBT;
-    public void $define(){
-        listBT = new ListPicker(this);
+public class BtPaired extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bt_paired);
+        final ListView btList= findViewById(R.id.bt_list);
+        
     }
 }
