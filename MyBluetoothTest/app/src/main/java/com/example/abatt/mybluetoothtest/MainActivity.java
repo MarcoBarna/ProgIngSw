b7f375a007c2fc03e50b2e09c77cafa445a74a96
AlviseB <37024233+AlviseB@users.noreply.github.com>
2018-10-27 19:13:57 +0200

    Added funcionality
    Added Listing for Paired Bluetooth Devices

--- MyBluetoothTest/app/src/main/java/com/example/abatt/mybluetoothtest/MainActivity.java
+++ MyBluetoothTest/app/src/main/java/com/example/abatt/mybluetoothtest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.abatt.mybluetoothtest;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,16 +12,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
     //Costanti per gestire le richieste ad altre Activity
     private final static int REQUEST_ENABLE_BT = 1;
+
+    BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        setupBluetoothReceiver();
+
+        checkBluetooth();
+
+    }
+    private void setupBluetoothReceiver() {
         //MainActivity ha un BroadcastReceiver per ascoltare il cambiamento di stato del Bluetooth
         MyBluetoothReceiver myBluetoothReceiver = new MyBluetoothReceiver();
         IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
@@ -28,15 +41,11 @@ public class MainActivity extends AppCompatActivity {
         //Abilito la ricezione del Broadcast definito da filter tramite il BroadcastReceiver myBluetoothReceiver
         //IMPORTANTE: ricevo il Broadcast solo quando l'app è attiva
         registerReceiver(myBluetoothReceiver, filter);
-
-        checkBluetooth();
-
     }
 
     //Controlla lo stato del Bluetooth e lo comunica su schermo
     private void checkBluetooth(){
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        TextView textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.bluetoothStatus);
 
         if (bluetoothAdapter != null) {
             textView.setText(R.string.bluetoothSupported);
@@ -52,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
     //Si occupa di chiedere all'utente di accendere il Bluetooth se è spento, altrimenti avvisa che il Bluetooth è già acceso
     public void askBluetooth(View view){
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        TextView textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.bluetoothStatus);
         if (bluetoothAdapter.isEnabled())
             textView.setText(R.string.bluetoothAlreadyEnabled);
         else{
@@ -60,6 +69,26 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             textView.setText(R.string.bluetoothAsking);
         }
+    }
+
+    public void showPairedBluetoothDevice(View view){
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        TextView textView = findViewById(R.id.listOfPairedDevices);
+
+        if (bluetoothAdapter.isEnabled())
+            textView.setTextKeepState(getString(R.string.startPairedBluetoothDevices));
+        else
+            textView.setTextKeepState(getString(R.string.askEnableBluetoothForPairedBluetoothDevices));
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                textView.setText(textView.getText() + deviceName + " [" + deviceHardwareAddress + "]\n");
+            }
+        }
+
     }
 
     @Override
@@ -71,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
             case REQUEST_ENABLE_BT: //Ho ricevuto il risultato della richiesta al Bluetooth
                 if (resultCode == RESULT_OK) { //La richiesta è andata a buon fine (Bluetooth acceso)
                     BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-                    TextView textView = findViewById(R.id.textView);
+                    TextView textView = findViewById(R.id.bluetoothStatus);
                     if (bluetoothAdapter.isEnabled())
                         textView.setText(R.string.bluetoothHasBeenEnabled);
                     else
@@ -79,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
                 } else
                     //La richiesta è stata annullata (L'utente non ha accettato la richiesta di accendere il Bluetooth)
                     if (resultCode == RESULT_CANCELED) {
-                        TextView textView = findViewById(R.id.textView);
+                        TextView textView = findViewById(R.id.bluetoothStatus);
                         textView.setText(R.string.bluetoothRequestCANCELED);
                 }
                 break;
@@ -95,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
             //Estraggo lo stato attuale da intent
             int currentState = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.STATE_OFF);
             //Prendo il collegamento a textView (visibile perché questa è una inner class)
-            TextView textView = findViewById(R.id.textView);
+            TextView textView = findViewById(R.id.bluetoothStatus);
             //Confronto lo stato del Bluetooth con i valori di libreria e di conseguenza cambio il testo nella textView
             switch (currentState) {
                 case BluetoothAdapter.STATE_TURNING_ON: //In accensione
