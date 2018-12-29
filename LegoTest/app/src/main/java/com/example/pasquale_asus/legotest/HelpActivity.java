32108da480b0b6e3b77bdbfd68ce9bfd1bd99f9e
Marco Barnà <34555572+MarcoBarna@users.noreply.github.com>
2018-12-29 18:32:31 +0100

    Creato Credits

--- LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/HelpActivity.java
+++ LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/HelpActivity.java
@@ -1,20 +1,29 @@
 package com.example.pasquale_asus.legotest;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
+    Dialog dialog;
+    ImageView closePopup;
+    TextView titlePopup, marco, pasquale, riccardo, alvise;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -65,7 +74,23 @@ public class HelpActivity extends AppCompatActivity {
     }
 
     public void onButtonCreditsClick(View v){
-        //TODO activity and startActivity
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.epic_popup_custom);
+        closePopup = (ImageView) dialog.findViewById(R.id.closePopup);
+        titlePopup = (TextView) dialog.findViewById(R.id.titlePopup);
+        marco = (TextView) dialog.findViewById(R.id.marco);
+        pasquale = (TextView) dialog.findViewById(R.id.pasquale);
+        riccardo = (TextView) dialog.findViewById(R.id.riccardo);
+        alvise = (TextView) dialog.findViewById(R.id.alvise);
+
+        closePopup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             dialog.dismiss();
+            }
+        });
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
     }
 
 
