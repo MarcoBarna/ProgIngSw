9f6052c585db1b209e387a8c165ec0756921a890
@aboutpask <passpolo00@gmail.com>
2019-01-12 15:15:00 +0100

    add touchsensor object to class ev3sensor

--- LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/ElementsEV3.java
+++ LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/ElementsEV3.java
@@ -3,6 +3,7 @@ package com.example.pasquale_asus.legotest;
 import com.google.appinventor.components.runtime.BluetoothClient;
 import com.google.appinventor.components.runtime.Ev3Commands;
 import com.google.appinventor.components.runtime.Ev3Motors;
+import com.google.appinventor.components.runtime.Ev3TouchSensor;
 import com.google.appinventor.components.runtime.Form;
 
 //this class contains the elements for initialize the ev3
@@ -12,6 +13,7 @@ public class ElementsEV3 extends Form  {
     public Ev3Motors rightMotors;
     public BluetoothClient bluetoothClient;
     public Ev3Commands commands;
+    public Ev3TouchSensor touchSensor;
 
     public ElementsEV3(){
         $define();
@@ -21,5 +23,6 @@ public class ElementsEV3 extends Form  {
         leftMotors = new Ev3Motors(this);
         rightMotors = new Ev3Motors(this);
         bluetoothClient = new BluetoothClient(this);
+        touchSensor = new Ev3TouchSensor(this);
     }
 }
