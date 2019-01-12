9a56c523783d3b10fd36e34e3cd29bdd5c7a9cff
AlviseB <37024233+AlviseB@users.noreply.github.com>
2019-01-12 16:21:40 +0100

    Toolbar added to automaticDrive

--- LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/AutomaticDriveActivity.java
+++ LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/AutomaticDriveActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -40,6 +41,13 @@ public class AutomaticDriveActivity extends AppCompatActivity {
                 }.start();
             }
         });
+
+        //Sets up the Toolbar
+        Toolbar toolbar = findViewById(R.id.automaticDriveToolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
     }
 
