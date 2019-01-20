797bf43c74bb632cd115a6c37a602b463418607e
@aboutpask <passpolo00@gmail.com>
2019-01-20 18:24:13 +0100

    Branch alvise

--- LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/AutomaticDriveActivity.java
+++ LegoTest/app/src/main/java/com/example/pasquale_asus/legotest/AutomaticDriveActivity.java
@@ -2,21 +2,16 @@ package com.example.pasquale_asus.legotest;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
-import com.google.appinventor.components.runtime.Button;
 import com.google.appinventor.components.runtime.Ev3Motors;
 import com.google.appinventor.components.runtime.Ev3UltrasonicSensor;
-import com.google.appinventor.components.runtime.Form;
 
-import java.nio.charset.MalformedInputException;
-import java.text.Normalizer;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 public class AutomaticDriveActivity extends AppCompatActivity {
     private android.widget.Button firstaction, stopButton;
@@ -26,7 +21,11 @@ public class AutomaticDriveActivity extends AppCompatActivity {
     public static Thread readSensors;
     private TextView debug;
     Handler handler;
+<<<<<<< HEAD
     private boolean handlerStop;
+=======
+    private boolean handlerStop, threadStop;
+>>>>>>> ALVISe
     Runnable r = null;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,53 +40,46 @@ public class AutomaticDriveActivity extends AppCompatActivity {
         firstaction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //stopButton.setVisibility(View.VISIBLE);
+                debug.setVisibility(View.VISIBLE);
+                stopButton.setActivated(true);
                 interruptAction = false;
                 debug.setText("Cliccato");
                 handlerStop = true;
-
-                r = new Runnable() {
-                    public void run() {
-                        debug.setText(ultrasonicSensor.GetDistance() + "");
-                        if(ultrasonicSensor.GetDistance() > 20){
-                            motors.RotateSyncIndefinitely(100, 0);
-                        }
-                        else {
-                            motors.Stop(false);
-                            motors.RotateSyncIndefinitely(-50,90);
-                            //motors.RotateSyncInTachoCounts(-50, 2, 90, false);
-                        }
-                        if (handlerStop == false)
-                            handler.postDelayed(this, 200);
-                    }
-                };
-                handlerStop = false;
-                handler.postDelayed(r, 500);
-
+                avoidObstacles();
+                //Runnable algorithm = avoidObstacles();
             }
-
-
         });
+
         stopButton = findViewById(R.id.stop_button);
+        stopButton.setActivated(false);
+        debug.setVisibility(View.INVISIBLE);
+        //stopButton.setVisibility(View.INVISIBLE);
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 handler.removeCallbacks(r);
                 handlerStop = true;
+<<<<<<< HEAD
+=======
+                threadStop = true;
+>>>>>>> ALVISe
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         motors.Stop(false);
                     }
                 }, 500);
+<<<<<<< HEAD
+=======
+                //stopButton.setVisibility(View.INVISIBLE);
+                stopButton.setActivated(false);
+                debug.setVisibility(View.INVISIBLE);
+>>>>>>> ALVISe
             }
         });
 
-        Toolbar toolbar = findViewById(R.id.automaticDriveToolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
+        Utility.setupToolbar(this, R.id.automaticDriveToolbar);
     }
 
     @Override
@@ -115,18 +107,67 @@ public class AutomaticDriveActivity extends AppCompatActivity {
     }
 
     public void initializeMotors(){
-        ElementsEV3 elementsEV3 = new ElementsEV3();
-        motors = elementsEV3.leftMotors;
+        motors = MainActivity.ev3.outputs.leftMotors;
         motors.MotorPorts("BC");
-        motors.BluetoothClient(MainActivity.bluetoothClient);
-        ultrasonicSensor = elementsEV3.ultrasonicSensor;
+        motors.BluetoothClient(MainActivity.ev3.bluetoothClient);
+        ultrasonicSensor = MainActivity.ev3.inputs.ultrasonicSensor;
         ultrasonicSensor.SensorPort("3");
-        ultrasonicSensor.BluetoothClient(MainActivity.bluetoothClient);
+        ultrasonicSensor.BluetoothClient(MainActivity.ev3.bluetoothClient);
+    }
+    public void avoidObstacles(){
+        r = new Runnable() {
+            public void run() {
+                debug.setText("Distance value: "+ (int)ultrasonicSensor.GetDistance());
+                double distance = ultrasonicSensor.GetDistance();
+                if(distance > 0 && distance < 35){
+                    motors.Stop(false);
+                    motors.RotateSyncIndefinitely(-50,90);
+                    //motors.RotateSyncInTachoCounts(-50, 2, 90, false);
+                }
+                else {
+                    motors.RotateSyncIndefinitely(100, 0);
+                }
+                if (handlerStop == false)
+                    handler.postDelayed(this, 200);
+            }
+        };
+        handlerStop = false;
+        handler.postDelayed(r, 500);
     }
 
 
-
-
+//    public Runnable avoidObstacles(){
+//        threadStop = false;
+//        Runnable algorithm = new Runnable() {
+//            @Override
+//            public void run() {
+//                double distance = 0;
+//                try {
+//                    distance = MainActivity.ev3.inputs.readProximitySensor().get();
+//                } catch (InterruptedException | ExecutionException e) {
+//                    e.printStackTrace();
+//                }
+//                if (distance > 0 && distance < 20) {
+//                    handler.post(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            motors.RotateSyncInDuration(-50, 500, 100, false);
+//                        }
+//                    });
+//                } else {
+//                    handler.post(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            motors.RotateSyncIndefinitely(50, 0);
+//                        }
+//                    });
+//                }
+//                if (!handlerStop)
+//                    handler.postDelayed(this, 500);
+//            }
+//        };
+//        return algorithm;
+//    }
 
 
 }
